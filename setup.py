1a0a9768078a5dd27114be68264f69d3832344da
haanme <haanme@utu.fi>
2022-05-28 00:25:19 +0300

    Updating recent changes

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='ProstateFeatures',
+    version='1.1.0',
+    packages=[''],
+    url='https://github.com/haanme/ProstateFeatures',
+    license='LGPL 3.0',
+    author='Harri Merisaari',
+    author_email='haanme@utu.fi',
+    description='MRC Radiomic feature extraction package'
+    install_requires=[
+        'dipy',
+        'csv',
+        'scipy',
+        'skimage',
+        'trimesh',
+        'plyfile',
+        'cv2',
+        'nibabel',
+        'sklearn',
+    ]
+)
